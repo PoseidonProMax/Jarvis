91e7c10ca9372b930169e8e3e5add62bc5593330
PoseidonProMax <abhinavuk007@gmail.com>
2026-05-26 12:38:06 +0530

    Phase 2 complete - Jarvis protocol upgraded

--- listen.py
+++ listen.py
@@ -1,28 +1,22 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+os.environ['ALSA_CARD'] = '0'
+
 import speech_recognition as sr
 
 r = sr.Recognizer()
 
-# Calibrate microphone once
-with sr.Microphone() as source:
-    print("Calibrating microphone...")
-    r.adjust_for_ambient_noise(source, duration=1)
-
 def listen():
 
     with sr.Microphone() as source:
-        r.pause_threshold = 1
 
-        audio = r.listen(
-            source,
-            timeout=5,
-            phrase_time_limit=6
-        )
+        audio = r.listen(source)
 
     try:
 
         command = r.recognize_google(audio).lower()
 
-        print("You said:", command)
+        print(f"You said: {command}")
 
         return command
 
